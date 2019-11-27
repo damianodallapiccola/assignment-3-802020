9b836c6f863b4945aa879ce94b5d86aa53bfd6a9
damianodallapiccola <damiano.dallapiccola@gmail.com>
2019-11-27 14:46:47 +0200

    added java code

---
+++ code/java/src/main/java/customerstreamapp/StreamingJob.java
@@ -0,0 +1,89 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package customerstreamapp;
+
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+
+/**
+ * Skeleton for a Flink Streaming Job.
+ *
+ * <p>For a tutorial how to write a Flink streaming application, check the
+ * tutorials and examples on the <a href="http://flink.apache.org/docs/stable/">Flink Website</a>.
+ *
+ * <p>To package your application into a JAR file for execution, run
+ * 'mvn clean package' on the command line.
+ *
+ * <p>If you change the name of the main class (with the public static void main(String[] args))
+ * method, change the respective entry in the POM.xml file (simply search for 'mainClass').
+ */
+public class StreamingJob {
+
+	public static void main(String[] args) throws Exception {
+		// set up the streaming execution environment
+		final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+
+		// checkpointing is required for exactly-once or at-least-once guarantees
+		env.enableCheckpointing(...);
+
+		final RMQConnectionConfig connectionConfig = new RMQConnectionConfig.Builder()
+				.setHost("localhost")
+				.setPort(5000)
+				...
+    			.build();
+
+		final DataStream<String> stream = env
+				.addSource(new RMQSource<String>(
+						connectionConfig,            // config for the RabbitMQ connection
+						"queueName",                 // name of the RabbitMQ queue to consume
+						true,                        // use correlation ids; can be false if only at-least-once is required
+						new SimpleStringSchema()))   // deserialization schema to turn messages into Java objects
+				.setParallelism(1);             	// non-parallel source is only required for exactly-once
+
+
+
+
+
+
+
+
+
+		/*
+		 * Here, you can start creating your execution plan for Flink.
+		 *
+		 * Start with getting some data from the environment, like
+		 * 	env.readTextFile(textPath);
+		 *
+		 * then, transform the resulting DataStream<String> using operations
+		 * like
+		 * 	.filter()
+		 * 	.flatMap()
+		 * 	.join()
+		 * 	.coGroup()
+		 *
+		 * and many more.
+		 * Have a look at the programming guide for the Java API:
+		 *
+		 * http://flink.apache.org/docs/latest/apis/streaming/index.html
+		 *
+		 */
+
+		// execute program
+		env.execute("Flink Streaming Java API Skeleton");
+	}
+}
